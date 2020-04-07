6ff56cb17bf6a6e492cd160cd268c6de2487fbf1
ameyaZope <zopeameya@gmail.com>
2020-04-07 02:12:05 +0000

    Some syntax corrections and updated the README.md file

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # OnLoadScript
-A script for installing general stuff after clean install of Linux Operating System. 
+A script for installing general stuff after clean install of Linux Operating System.
+
+## How to run the script
+Open a new terminal and type the below two commands in the terminal 
+1. chmod +x onLoad.sh
+2. ./onLoad.sh
+
+The first command is to give execute permissions for the file onLoad.sh and the second command is to actually execute the command. Note that if the second command does not work then  try running this command : source ./onLoad.sh
+
+## What does the Script Do?
+The script mounts your C and D drive.
+The script then installs the following softwares
+1. Java
+2. Git
+3. Google Chrome
+
+The script then creates aliases for the following softwares.
+1. Sublime Text (alias : subl)
+2. Eclipse (alias : eclipse)
+
+The aliases are created by using the default location that is provided for the location of software on my computer. This default location can be overriden by providing the following command line input
+1. For Sublime Text : $3 as "SublimeText" and $4 as addressOfSublimeExecutable
+2. For eclipse : $5 as "Eclipse" and $6 as addressOfEclipseExecutable
--- onLoad.sh
+++ onLoad.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 #Author Ameya Zope
 
 #Mounting Drive C
-sudo mkdir /media/ubuntu/Volume\ C -parent
+sudo mkdir /media/ubuntu/Volume\ C --parent
 sudo mount /dev/sda2 /media/ubuntu/Volume\ C 
 
 #Mounting Drive D
-sudo mkdir /media/ubuntu/Volume\ D -parent
+sudo mkdir /media/ubuntu/Volume\ D --parent
 sudo mount /dev/sda3 /media/ubuntu/Volume\ D
 
 
@@ -18,10 +18,10 @@ sudo apt-get install default-jdk --yes
 
 sudo snap install vlc --yes
 
-export defaultLoactionOfGoogleChrome="/media/ubuntu/Volume\ D/Ubuntu\ Install/google-chrome-stable_current_amd64.deb" 
-export addressOfGoogleChrome="Google Chrome Location Here"
+defaultLoactionOfGoogleChrome="/media/ubuntu/Volume\ D/Ubuntu\ Install/google-chrome-stable_current_amd64.deb" 
+addressOfGoogleChrome="/media/ubuntu/Volume\ D/Ubuntu\ Install/google-chrome-stable_current_amd64.deb"
 echo $addressOfGoogleChrome 
-if [ $1 eq "GoogleChrome" ] 
+if [ $# -ge 2 ] && [ $1 = "GoogleChrome" ] 
 	then
 		$addressOfGoogleChrome={2:-$defaultLoactionOfGoogleChrome}
 	else
@@ -31,23 +31,27 @@ fi
 sudo dpkg -i ${addressOfGoogleChrome}
 
 #Adding Alias for SublimeText
-export addressOfSublimeText="Addres of sublime Text Here"
-export defultLocationOfSublimeText="/media/ubuntu/Volume\ D/Ubuntu\ Install/sublime_text_3/sublime_text"
-if [ $3 eq "SublimeText" ] 
+addressOfSublimeText="/media/ubuntu/Volume\ D/Ubuntu\ Install/sublime_text_3/sublime_text"
+defaultLocationOfSublimeText="/media/ubuntu/Volume\ D/Ubuntu\ Install/sublime_text_3/sublime_text"
+# To check equality sign among numbers use -eq/==. To check equality sign among strings use =
+# refer : https://www.tutorialspoint.com/unix/unix-string-operators.htm for string comparison
+# refer : https://www.tutorialspoint.com/unix/unix-basic-operators.htm for numer comparison
+if [ $# -ge 4 ] && [ $3 = "SublimeText" ]  
 	then
-		$addressOfSublimeText={4:-$defaultLocationOfSublimeText}
+		addressOfSublimeText=${4:-defaultLocationOfSublimeText}
+		echo "First"
 	else
-		$addressOfSublimeText=$defultLocationOfSublimeText
+		addressOfSublimeText=$defaultLocationOfSublimeText
 fi
 alias subl=$addressOfSublimeText
 
-export addressOfEclipse="Address of eclipse Here"
-export defaultLocationOfEclipse="/media/ubuntu/Volume\ D/Ubuntu\ Install/eclipse/eclipse"
-if [ $5 eq "Eclipse" ] 
+addressOfEclipse="Address of eclipse Here"
+defaultLocationOfEclipse="/media/ubuntu/Volume\ D/Ubuntu\ Install/eclipse/eclipse"
+if [ $# -ge 6 ] && [ $5 = "Eclipse" ] 
 	then
-		$addressOfEclipse={6:-$defaultLocationOfEclipse}
+		addressOfEclipse=${6:-defaultLocationOfEclipse}
 	else
-		$addressOfEclipse=$defaultLocationOfEclipse
+		addressOfEclipse=$defaultLocationOfEclipse
 alias subl=$addressOfEclipse
 
 #Configuring git
@@ -58,3 +62,24 @@ git config --global user.anme "ameyaZope"
 alias l='ls -CF'
 alias la='ls- A'
 alias ll = 'ls -alF'
+
+# Logical Operator in bash
+# && AND
+
+# if [ $condition1 ] && [ $condition2 ]
+# #  Same as:  if [ $condition1 -a $condition2 ]
+# #  Returns true if both condition1 and condition2 hold true...
+
+# if [[ $condition1 && $condition2 ]]    # Also works.
+# #  Note that && operator not permitted inside brackets
+# #+ of [ ... ] construct.
+
+# || OR
+
+# if [ $condition1 ] || [ $condition2 ]
+# # Same as:  if [ $condition1 -o $condition2 ]
+# # Returns true if either condition1 or condition2 holds true...
+
+# if [[ $condition1 || $condition2 ]]    # Also works.
+# #  Note that || operator not permitted inside brackets
+# #+ of a [ ... ] construct.
